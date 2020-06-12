9d3b1e8ed3f52dc5d093cbd96996c01af84fc330
TheFruitDude <36520284+TheFruitDude@users.noreply.github.com>
2020-06-12 15:16:38 +0200

    Add files via upload

---
+++ Search _The_Web/Search _The_Web/Form1.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtsearch = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lb1 = New System.Windows.Forms.ListBox()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(204, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Hello. I can make advanced Web Search"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("AntiqueOliveBlack", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.btnSearch.Location = New System.Drawing.Point(400, 152)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(172, 62)
+        Me.btnSearch.TabIndex = 1
+        Me.btnSearch.Text = "Crawl The Web"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'txtsearch
+        '
+        Me.txtsearch.Location = New System.Drawing.Point(12, 36)
+        Me.txtsearch.Name = "txtsearch"
+        Me.txtsearch.Size = New System.Drawing.Size(599, 20)
+        Me.txtsearch.TabIndex = 2
+        Me.txtsearch.Text = "neural networks natural image processsing"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Options:"
+        '
+        'lb1
+        '
+        Me.lb1.FormattingEnabled = True
+        Me.lb1.Location = New System.Drawing.Point(12, 91)
+        Me.lb1.Name = "lb1"
+        Me.lb1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.lb1.Size = New System.Drawing.Size(329, 186)
+        Me.lb1.TabIndex = 4
+        '
+        'txtResult
+        '
+        Me.txtResult.Location = New System.Drawing.Point(12, 311)
+        Me.txtResult.Multiline = True
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.Size = New System.Drawing.Size(610, 422)
+        Me.txtResult.TabIndex = 6
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Location = New System.Drawing.Point(15, 292)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(45, 13)
+        Me.lbl3.TabIndex = 7
+        Me.lbl3.Text = "Results:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(674, 760)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.lb1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtsearch)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Advanced Web Search"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents txtsearch As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lb1 As ListBox
+    Friend WithEvents txtResult As TextBox
+    Friend WithEvents lbl3 As Label
+End Class
